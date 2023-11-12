19a6d88c015dd5ed9fca800f709a3a39f470e866
Isabelle Wang <ilwang@smith.edu>
2023-11-11 19:35:47 -0500

    okay

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ class test:
     def main(): 
         myfridge = Refrigerator(); 
 
-        choice = choice = input("What would you like to do? Press: \n 0. Exit Program \n1.Add produce \n 2.Print items in fridge\n 3.Remove produce \n")
+        choice = input("What would you like to do? Press: \n 0. Exit Program \n 1.Add produce \n 2.Print items in fridge \n 3.Remove produce \n")
 
-        while(choice != 0) : 
-            choice = input("What would you like to do? Press: \n 0. Exit Program \n1.Add produce \n 2.Print items in fridge\n 3.Remove produce \n")
-            
-            if(choice == 1):    
+        exit_menu = False
+        while not exit_menu: 
+            if "0" in choice:
+                exit()
+            elif "1" in choice:    
                 item = input("What produce would you like to put in the fridge?\n")
                 item_num = int(input("What is the number of items you would like to store in fridge\n"))
                 exp_day = input("How many days until this produce expire?\n")
@@ -19,21 +20,18 @@ class test:
         
                 myfridge.add_item(product, item_num); 
                 print(str(item_num), item, " was added into your refridgerator!"); 
-            elif (choice == 2) :
+            elif "2" in choice:
                 myfridge.print_items() 
-            elif (choice == 3): 
-                option = input("What would you like to remove? Press \n 1. an expired item \n 2. item of your choice")
-                if(option == 1): 
+            elif "3" in choice: 
+                option = input("What would you like to remove? Press \n 1. an expired item \n 2. item of your choice \n")
+                if "1" in option: 
                     myfridge.remove_expired()
-                elif (option == 2): 
+                elif "2" in option: 
                     prod = input("What produce would you like to remove?")
                     myfridge.remove_item(Produce(product))
             else :
                 print("Error. Please choose a number 0 to 3.")
-            
-    
-    
-    
-        
-        
+            exit_menu = True  
+
+        choice = input("What would you like to do? Press: \n 0. Exit Program \n1.Add produce \n 2.Print items in fridge\n 3.Remove produce \n")
     main()
